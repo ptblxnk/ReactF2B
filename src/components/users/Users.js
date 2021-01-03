543c16b1f3cb381fea40a96d56ad81f5216e6572
ptblxnk <30389498+ptblxnk@users.noreply.github.com>
2021-01-03 14:35:30 -0800

    Update #3 - Passing State w/Props
    - Passed "UserItem" component through users to loop to show different users within the array. Assigning a key to UserItems ensures that users are display only once.
    
    - Removed state from "UserItem" component since we are passing state via props.
    
    - Applied grid layout to user component by style attribute  to with variable
    
    - Changed 'this.state' to 'this.props' for object destructuring since state is being passed using props (UserItem component)
    
    - Passed "User" component through app.js
    
    - Wrapped all users in a container to correct grid layout

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import UserItem from "./UserItem";
 class Users extends Component {
   state = {
     users: [
@@ -26,13 +26,19 @@ class Users extends Component {
 
   render() {
     return (
-      <div>
+      <div style={userStyle}>
         {this.state.users.map((user) => (
-          <div key={user.id}>{user.login}</div>
+          <UserItem key={user.id} user={user} />
         ))}
       </div>
     );
   }
 }
+
+const userStyle = {
+  display: "grid",
+  gridTemplateColumns: "repeat(3, 1fr)",
+  gridGap: "1rem",
+};
 
 export default Users;
