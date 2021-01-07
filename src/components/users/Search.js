7cdc54cfa10cd1fde03890b316a6b27dafd7651b
ptblxnk <30389498+ptblxnk@users.noreply.github.com>
2021-01-06 16:03:40 -0800

    Update #11 - Created "Alert" Component
    - Created an alert component for the app in the case a request is sent to the server but nothing is in the search input field. Pass through app as well.
    - Destructured props in "Search.js"

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -10,13 +10,19 @@ class Search extends Component {
     searchUsers: PropTypes.func.isRequired,
     clearUsers: PropTypes.func.isRequired,
     showClear: PropTypes.bool.isRequired,
+    setAlert: PropTypes.func.isRequired,
   };
 
   onChange = (e) => this.setState({ [e.target.name]: e.target.value });
+
   onSubmit = (e) => {
     e.preventDefault();
-    this.props.searchUsers(this.state.text);
-    this.setState({ text: "" });
+    if (this.state.text === "") {
+      this.props.setAlert("Please enter something", "light");
+    } else {
+      this.props.searchUsers(this.state.text);
+      this.setState({ text: "" });
+    }
   };
 
   render() {
@@ -37,7 +43,7 @@ class Search extends Component {
             className="btn btn-dark btn-block"
           />
         </form>
-        {this.props.showClear && (
+        {showClear && (
           <button
             onClick={clearUsers}
             showClear={showClear}
